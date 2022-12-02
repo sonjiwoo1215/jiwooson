656354e0ae9d2bba31fd295c76368c30c8438470
sonjiwoo1215 <jiwoo211500@naver.com>
2022-12-02 15:45:10 +0900

    d
    d

---
+++ www/sketch.js
@@ -0,0 +1,35 @@
+var wave;
+var playing = false;
+var button;
+
+
+
+function setup() {
+  createCanvas(640,480);
+  wave = new p5.Oscillator();
+  wave.setType('sine');
+  
+  wave.freq(440);
+  wave.amp(0.1);
+  
+  button = createButton('play/pause');
+  button.mousePressed(toggle);
+
+}
+
+function draw() {
+  
+}
+function toggle(){
+
+  if(!playing){
+    wave.start();
+    wave.amp(0.5,1);
+    playing=true;
+  
+  }
+  else{
+    wave.amp(0,1);
+    playing = false;
+  }
+}
